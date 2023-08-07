9f17058416c82d1328eef161c3b313904b33de6d
Makoto Sato <sato.makoto@nabla-mobility.com>
2023-08-07 12:40:18 +0900

    Get IBMBRACEORJP from onclick

--- pyjpboatrace/operator/static.py
+++ pyjpboatrace/operator/static.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 from ..certification import check_login_status, login
-from ..const import IBMBRACEORJP
+from ..const import BOATRACEJP_MAIN_URL
 from ..user_information import UserInformation
 
 
@@ -29,16 +29,22 @@ def visit_ibmbraceorjp(
         login(driver, user, timeout)
 
     # visit
-    driver.get(IBMBRACEORJP)
+    driver.get(BOATRACEJP_MAIN_URL)
+
+    element = driver.find_element(By.ID, "commonHead")
+    ibmbraceorjp_url_path = element.get_attribute("onclick").split("'")[1]
+    url = f"{BOATRACEJP_MAIN_URL}{ibmbraceorjp_url_path}"
+
+    driver.get(url)
 
     # check
-    return driver.title == 'トップ - BOAT RACE インターネット投票'
+    return driver.title == "トップ - BOAT RACE インターネット投票"
 
 
 def get_bet_limit(
     user: UserInformation,
     driver: webdriver.remote.webdriver.WebDriver,
-    timeout: int = 15
+    timeout: int = 15,
 ) -> int:
     """Get the amount of deposit.
 
@@ -56,8 +62,8 @@ def get_bet_limit(
 
     # get vote limit
     WebDriverWait(driver, timeout).until(
-        EC.presence_of_element_located((By.ID, 'currentBetLimitAmount'))
+        EC.presence_of_element_located((By.ID, "currentBetLimitAmount"))
     )
-    limit = driver.find_element_by_id('currentBetLimitAmount').text
+    limit = driver.find_element(By.ID, "currentBetLimitAmount").text
 
-    return int(limit.replace(',', ''))
+    return int(limit.replace(",", ""))
